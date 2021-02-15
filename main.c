4f2db0e9037b711456802caa9c4c1905a5816de8
Sedad1337 <sedad1501@hotmail.com>
2021-02-15 09:54:06 +0100

    Fehlerbehebung

--- main.c
+++ main.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-int day_of_the_year(int day, int month, int year)
-{
-
-    return 0;
-}
-
-int main()
-{
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
-    return 0;
-}
-#include <stdio.h>
-#include <stdlib.h>
 //Sedad Deeg
 //Programm zur Bestimmung des Jahrestages
 //Erweiterung, Auslagerung des Codes in Funktionen
