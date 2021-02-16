3761b728c763c03c7cd08978329dd20903eed3ff
Sedad1337 <sedad1501@hotmail.com>
2021-02-16 17:55:48 +0100

    Test function Datei

---
+++ functions.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Berechnung() {
+    //Funktion, welche für die Berechnung zuständig ist
+
+    for(int i = 1; i < getMonthOfYear(); i++)  //Prüft ob der Monat komplett durchgelaufen ist , falls ja werden die Tage draufgerechnet
+        {
+            getDayOfMonth() += ArrayMonthDays();
+            //Monatstage[i];
+        }
+
+        if(getCurrentYear() %4 == 0)   //Prüft ob es ein Schaltjahr ist
+        {
+            if(getCurrentYear() %100 == 0)
+            {
+                if(getCurrentYear() %400 == 0)
+                    getIsLeapYear() = 1;
+                else
+                    getIsLeapYear() = 0;
+            }
+            else    getIsLeapYear() = 1;
+        }
+        else    getIsLeapYear() = 0;
+
+        if (getIsLeapYear() == 1)   //Addiert einen drauf, wenn es ein Schaltjahr ist
+        {
+            //TagImMonat += 1;
+        }
+}
+
+void Ausgabe() {
+    //Funktion, welche das Ergebnis ausgibt
+    printf("Result: It's the %i. day of the year",TagImMonat);
+}
+
+int getDayOfMonth(){
+    int DayOfMonth = 0;
+    return DayOfMonth;
+}
+int getMonthOfYear(){
+    int MonthOfYear = 0;
+    return MonthOfYear;
+}
+int getCurrentYear(){
+    int CurrentYear = 0;
+    return CurrentYear;
+}
+int getIsLeapYear(){
+    int IsLeapYear = 0;
+    return IsLeapYear;
+}
+void ArrayMonthDays() {
+    //Funktion, welche die Arraywerte initialisiert
+    int Monatstage[12]; //Array der Größe 12, entspricht der Anzahl Monate
+    Monatstage[1]=31;
+    Monatstage[2]=28;
+    Monatstage[3]=31;
+    Monatstage[4]=30;
+    Monatstage[5]=31;
+    Monatstage[6]=30;
+    Monatstage[7]=31;
+    Monatstage[8]=31;
+    Monatstage[9]=30;
+    Monatstage[10]=31;
+    Monatstage[11]=30;
+    Monatstage[12]=31;
+    return Monatstage;
+}
--- main.c
+++ main.c
@@ -4,84 +4,29 @@
 //Programm zur Bestimmung des Jahrestages
 //Erweiterung, Auslagerung des Codes in Funktionen
 
-int TagImMonat = 0;
-int MonatImJahr = 0;
-int AktuellesJahr = 0;
-int ist_schaltjahr = 0;
-
 int main() {
     //Aufruf der einzelnen Funktionen
-    ArrayMonatstage();
-    imput_date();
+    ArrayMonthDays();
+    input_date();
     Berechnung();
     Ausgabe();
 }
 void input_date(int *pointerYear, int *pointerMonth, int *pointerDay) {
     //Funktion zur Eingabe der Werte Tag, Monat & Jahr
-    
+
     do {
-        printf("Tag eingeben\n");
+        printf("Enter day\n");
         scanf("%i",&TagImMonat);
-        printf("Monat eingeben\n");
+        printf("Enter month\n");
         scanf("%i",&MonatImJahr);
-        printf("Jahr eingeben\n");
+        printf("Enter year\n");
         scanf("%i",&AktuellesJahr);
-    
+
         if(exists_date(TagImMonat,MonatImJahr,AktuellesJahr) !=1) {
             printf("\nInput is not a valid date.Please try again.\n\n\n");
         }
 
     }
-    while (exists_date(TagImMonat,MonatImJahr,AktuellesJahr) !=1)
+    while (exists_date(TagImMonat,MonatImJahr,AktuellesJahr) !=1);
 }
 
-void Berechnung() {
-    //Funktion, welche für die Berechnung zuständig ist
-
-    for(int i = 1; i < MonatImJahr; i++)  //Pr�ft ob der Monat komplett durchgelaufen ist , falls ja werden die Tage draufgerechnet
-        {
-            TagImMonat += ArrayMonatstage();
-            //Monatstage[i];
-        }
-
-        if(AktuellesJahr %4 == 0)   //Prüft ob es ein Schaltjahr ist
-        {
-            if(AktuellesJahr %100 == 0)
-            {
-                if(AktuellesJahr %400 == 0)
-                    ist_schaltjahr = 1;
-                else
-                    ist_schaltjahr = 0;
-            }
-            else    ist_schaltjahr = 1;
-        }
-        else    ist_schaltjahr = 0;
-
-        if (ist_schaltjahr == 1)   //Addiert einen drauf, wenn es ein Schaltjahr ist
-        {
-            //TagImMonat += 1;
-        }
-}
-
-void ArrayMonatstage() {
-    //Funktion, welche die Arraywerte initialisiert
-    int Monatstage[12]; //Array der Größe 12, entspricht der Anzahl Monate
-    Monatstage[1]=31;
-    Monatstage[2]=28;
-    Monatstage[3]=31;
-    Monatstage[4]=30;
-    Monatstage[5]=31;
-    Monatstage[6]=30;
-    Monatstage[7]=31;
-    Monatstage[8]=31;
-    Monatstage[9]=30;
-    Monatstage[10]=31;
-    Monatstage[11]=30;
-    Monatstage[12]=31;
-    return Monatstage;
-}
-
-void Ausgabe() {
-    //Funktion, welche das Ergebnis ausgibt
-    printf("Es ist der %i. Tag im jahr",TagImMonat);
-}
