646dd289af7e5a9265e544a31512bcc4367a6e2a
Sedad1337 <sedad1501@hotmail.com>
2021-03-11 10:12:35 +0100

    Neu-Strukturierung des Programms

--- functions.h
+++ functions.h
@@ -1,89 +1,106 @@
-//-- Funktionen nach Spezifikation -- //
-
-//
-int day_of_the_year(struct dateStruct date) {
-
-int counter = 0;
-
-
- for(int i = month - 2; i >= 0; i--)
-    {
-        sum_of_days += days_per_month[i];
-    }
-    // Tage des aktuell angegebenen Monats.
-    sum_of_days += day;
-    // Ausgabe.
-    printf("Das Jahr hat bereits %i Tage", sum_of_days);
-
-    return 0;
-
-
-
-    //Prüfung der Gültigkeit des Jahres.
-    if(exists_date == 0) {
-        return -1; // -1 wird zurückgegeben wenn das Datum ungültig ist.
-    }
-}
-
-//Funktion zur Eingabe der Werte Tag, Monat, Jahr
-void input_date(dateStruct) {
-
-    struct dateStruct InputDate;
-
-    do {
-        //Eingabe der Werte durch den User
-        printf("Enter day\n");
-        scanf(main(DayOfMonth));
-        printf("Enter month\n");
-        scanf(month);
-        printf("Enter year\n");
-        scanf(year);
-
-        if(exists_date(month,month,year) !=1) { //Prüfung, ob das Jahr gültig ist.
-            printf("\nInput is not a valid date. Please try again.\n\n");
-        }
-
-    }
-    while (exists_date(day,month,year) !=1); //Wird solange durchlaufen, bis die Eingabe gültig ist.
-}
-
 //Funktion die feststellt, ob das eingegebene Jahr ein Schaltjahr ist
-//Gibt einen Rückgabewert nach Prüfung zurück
-int is_leapyear(struct dateStruct date) {
-
-    if(date.year >= 1582) { //Prüft, ob das eingegebene Jahr gleich oder über 1582 liegt.
-
-        if (date.year % 4 == 0 && date.year % 100 != 0) || (date.year % 400 == 0) { //Prüft die Bedingung des Schaltjahres
-            return 1; // 1 wird zurückgegeben, wenn year ein Schaltjahr ist.
-        } else {
-            return 0; // 0 wird zurückgegeben, wenn year kein Schaltjahr ist.
-    } else {
-        return -1; // -1 wird zurückgegeben, wenn das Jahr ungültig ist.
+//Gibt einen Integer als Rückgabewert nach Prüfung zurück
+int is_leapyear(int year)
+{   //Prüfung der Gültigkeit des Jahres
+    if(year < 1582 || year > 2400)
+    {
+        return -1;
+    }
+    //Prüfung, ob das Jahr ein Schaltjahr ist
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+            if(year%400==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        return 0;
     }
 }
 
-//Funktion zum Erzeugen des Arrays, mit den Monatstagen als Werte.
-int get_days_for_month(struct dateStruct date) {
-
-    int days_per_month[12] = {31,28,31,30,31,30,31,31,30,31,30,31}; //Array mit den Tagen des Monats als Werte
-
-    //Prüft, ob das Jahr ein Schaltjahr ist
-    if (is_leapyear(date.year) == 1) {
-        days_per_month[1] = 29; //Setzt die zweite Indexstelle (Februar) auf 29, wenn es ein Schaltjahr ist.
+//Funktion prüft ob der Monat existiert und gibt den höchstmöglichen Tag zurück
+int get_days_for_month(int year,int month)
+{
+    if(month < 1 || month > 12)
+    {
+        return -1;
     }
-    if (date.month > 12 || date.month < 1 || is_leapyear == -1) {
-        return -1; // -1 wird zurückgegeben, wenn ein ungültiger Monat oder ein üngültiges Jahr eingegeben wurde.
-    } else {
-        return days_per_month[month+1]; //Gibt die Tage (Indexwert des Arrays) des jeweiligen Monats zurück.
+    else
+    {
+        if(is_leapyear(year) == 1)
+        {   //Mögliche Tage, wenn es EIN Schaltjahr ist (Bzgl. zweiter Indexwert)
+            int days[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+            return days[month - 1];;
+        }
+        else
+        {   //Mögliche Tage, wenn es KEIN Schaltjahr ist (Bzgl. zweiter Indexwert)
+            int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+            return days[month - 1];
+        }
     }
 }
 
-//Funktion überprüft, ob das eingegebene Datum gültig ist.
-//Alles vor dem 1.1.1582 und nach dem 31.12.2400 ist ungültig.
-int exists_date(struct dateStruct date) {
+//Die Funktion prüft, ob der Tag existiert und ob das Jahr sich im gültigen Bereich befindet.
+//Die Funktion gibt je nach Fall 1 oder 0 zurück.
+int exist_date(int year, int month,int day)
+{
+    if (day <= 0 || day > get_days_for_month(year, month) || year < 1582|| year > 2400)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 
-    if(date.year < 1582 || date.year > 2400) { //Prüfung des Jahres
-        return 0; // 0 wird zurückgegeben, wenn das Jahr ungültig ist.
-    } else {
-        return 1; // 1 wird zurückgegeben, wenn das Jahr gültig ist.
+//Funktion berechnet den Tag des Jahres
+//Die Funktion gibt die Tage als Integer zurück
+int day_of_the_year(int year, int month, int day)
+{   //Prüfung, ob das eingegebene Datum existiert.
+    if (exist_date(year,month,day) == 1)
+    {
+        int i;
+        int TotalAmountDays = 0;
+
+        for(i=1; i<month; i++)
+        {   //Fügt den Tag dem aktuellen Datum hinzu
+            TotalAmountDays += get_days_for_month(year, i);
+        }
+        //Fügt den Tag dem aktuellen Datum hinzu
+        TotalAmountDays += day;
+        return TotalAmountDays;
+
+    }
+}
+//Funktion zur Nutzereingabe des Datums.
+//Eingegeben werden Tag, Monat, Jahr.
+void input_date(int *yearPointer, int *monthPointer, int *dayPointer){
+    printf("--------------------------------------------------------------\n");
+    printf("|                     -[Tag des Jahres]-                     |\n");
+    printf("|                    (Autor: Sedad Deeg)                     |\n");
+    printf("| Geben Sie die Werte an, um den Tag des Jahres zu berechnen |\n");
+    printf("--------------------------------------------------------------\n");
+    do{
+    printf("-> Tag: ");
+    scanf("%i", &*dayPointer);
+    printf("-> Monat: ");
+    scanf("%i", &*monthPointer);
+    printf("-> Jahr: ");
+    scanf("%i", &*yearPointer);
+    }while(exist_date(*yearPointer, *monthPointer, *dayPointer) == 0);
+    printf("------------------------------------\n");
 }
--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Sedad Deeg
-//Programm zur Bestimmung des Jahrestages
-//Erweiterung, Auslagerung des Codes in Funktionen
+#include <stdbool.h>
+#include "functions.h" //Inkludiert die Library functions.h
+/*
+Sedad Deeg
+Programm zur Bestimmung des Jahrestages.
+Auf Basis des vom Nutzer eingegebenen Datums.
+*/
 
 struct dateStruct {
     int year;
@@ -10,20 +14,18 @@ struct dateStruct {
     int day;
 };
 
-int main() {
-
-    int IsLeapYear = 0;
+int main()
+{
     int year = 0;
     int month = 0;
     int day = 0;
 
-    int *pointerDay = &day;
-    int *pointerMonth = &month;
-    int *pointerYear = &year;
+    //Aufruf der Funktion input_date in der Functions-Datei
+    input_date(&year, &month, &day);
 
-    input_date();
-    is_leapyear();
 
+    //Ausgabe des Tag des Jahres
+    //Gibt das Ergebnis der Funktion day_of_the_year aus.
+    printf("-> Tag des Jahres: %i. Tag.\n",day_of_the_year(year, month, day));
+    return 0;
 }
-
-
